6764049e7b0531242ffd73da059f0335a746d656
nimigeanconstantinion <constantin.nimigean@gmail.com>
2024-07-19 06:21:58 +0300

    cu 127.0.0.1

--- src/Api.tsx
+++ src/Api.tsx
@@ -12,7 +12,7 @@ export default class Api{
 
     api<T, U>(path: string, method = "GET", body: U): Promise<HttpResponse<T>> {
 
-        const bpath="http://localhost:5000"
+        const bpath="http://127.0.0.1:5000"
         const url =bpath+path;
 console.log(url);
         const options: RequestInit = {
